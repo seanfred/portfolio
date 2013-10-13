867e4a653e17793560dc3fb04f52d65b53692cd6
Sean <sean@Seans-Mac.local>
2013-10-13 16:12:23 -0700

    passing specs

--- test/features/accessing_home_page_test.rb
+++ test/features/accessing_home_page_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 feature "AccessingHomePage" do
   scenario "displays the welcome message" do
     visit root_path
-    page.text.must_include "Hello Code Fellows"
+    page.text.must_include "Sean Frederick"
   end
 end
