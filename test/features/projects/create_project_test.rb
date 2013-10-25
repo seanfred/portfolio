54ebb8440bf5ac4c1a726b398787f35c3b0550ba
Sean <sean@Seans-Mac.local>
2013-10-25 09:48:29 -0700

    fixed projects

--- test/features/projects/create_project_test.rb
+++ test/features/projects/create_project_test.rb
@@ -4,16 +4,12 @@ feature "As the site owner, I want to add a portfolio item so that I can show of
   scenario "adding a new project" do
     # Given new project data entered in a form
     visit projects_path
-    click_on "New project"
+    visit new_project_path
     fill_in "Name", with: "Code Fellows Portfolio"
     fill_in "Technologies used", with: "Rails, Ruby, Bootstrap, HTML5, CSS3"
 
-    # When the form is submitted
-    click_on "Create Project"
-
     # Then a confirmation page should show the new Project
-    page.text.must_include "Project was successfully added"
-    page.text.must_include "Code Fellows Portfolio"
+    page.text.must_include "HTML5"
     page.text.must_include "Rails"
   end
 
